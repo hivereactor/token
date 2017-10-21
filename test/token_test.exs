9775884d569514ad848cd70877801511e3d8e545
Adrian Daras <adrian@hivereactor.com>
2017-10-21 20:39:22 +0200

    Update tests

--- test/token_test.exs
+++ test/token_test.exs
@@ -1,5 +1,5 @@
 defmodule TokenTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   test("generated token is a string") do
     Token.generate!()
@@ -8,16 +8,16 @@ defmodule TokenTest do
   end
 
   test("generated token is 64 characters long") do
-    length = Token.generate!() |> String.length()
+    token = Token.generate!()
 
-    assert(length == 64)
+    assert(String.length(token) === 64)
   end
 
   test("hash is generated based on a string, not randomness") do
     result = "1686e3e0106632a8ecb5d10bb0bbea1ad75365972a549a67db351e6cf070095db6d1aae24b3ccb121e7c44626f0eb24e57c043056baba911722f1d571c68ef48"
     text = "test_string"
 
-    assert(Token.hash!(text) == result)
+    assert(Token.hash!(text) === result)
   end
 
   test("hashed token is a string") do
@@ -28,8 +28,8 @@ defmodule TokenTest do
   end
 
   test("hashed token (sha512) is 128 chars long") do
-    length = Token.generate!() |> Token.hash!() |> String.length()
+    hash = Token.hash!(Token.generate!())
 
-    assert(length === 128)
+    assert(String.length(hash) === 128)
   end
 end
